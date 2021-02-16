7a5d2a4ab3d4c5b27982a68b31e826a0b10ceecd
Dustin Fox <dustinfox1996@gmail.com>
2021-02-16 09:56:06 -0500

    Add project files.

---
+++ Banking2/Accounts2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking2 {
+    class Accounts2 {
+        public string AccountNumber { get; set; }
+        public string RoutingNumber { get; set; }
+        public string Descrtipion { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; } = 0;
+        public decimal MoneyDeposited { get; set; }
+        public decimal MoneyWithdrawn { get; set; }
+
+        public void deposit(decimal amount) {
+            if(amount > 0) {
+            Balance += amount;
+
+            }
+            //if(amount < 0) {
+            //    return;
+            //}
+           
+        }
+            public void withdraw(decimal amount) {
+            if(amount < 0) {
+                
+                Console.WriteLine($"go scam another bank");
+                return;
+            }
+
+            if (Balance >= amount) {
+                Balance -= amount;
+
+            } else {
+                Console.WriteLine($"No money for poor people");
+            }
+            
+            }
+        
+    }
+}
---
+++ Banking2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Banking2 {
+    class Program {
+        static void Main(string[] args) {
+            var acct1 = new Accounts2(); 
+            acct1.AccountNumber = "200";
+            acct1.deposit(420);
+            Console.WriteLine($"The Account number is {acct1.AccountNumber} And the balance after deposit is {acct1.Balance}");
+            acct1.withdraw(69);
+            Console.WriteLine($"The Account number is {acct1.AccountNumber} And the balance withdraw is {acct1.Balance}");
+            acct1.withdraw(420);
+            Console.WriteLine($"The Account number is {acct1.AccountNumber} And the balance withdraw is {acct1.Balance}");
+            acct1.withdraw(-300);
+            acct1.deposit(1.50m);
+            Console.WriteLine($"The Account number is {acct1.AccountNumber} And the balance after deposit is {acct1.Balance}");
+            acct1.deposit(-100);
+            Console.WriteLine($"The Account number is {acct1.AccountNumber} And the balance after deposit is {acct1.Balance}");
+
+
+
+
+        }
+    }
+}
